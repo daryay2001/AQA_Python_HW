58f100f48a647b453fe299a11572bd350eda27bc
Daria_Yakymova <yakymovadaria@gmail.com>
2023-09-11 17:27:48 +0300

    task1 was improved

--- Lesson_6/task1.py
+++ Lesson_6/task1.py
@@ -5,12 +5,12 @@
 import csv
 import os
 
-usd_rate = 36.95
+grn_rate = 36.95
 
-grn_salary = "test_file.csv"
+usd_salary = "test_file.csv"
 
 try:
-    with open(grn_salary, "r") as file:
+    with open(usd_salary, "r") as file:
         lines = file.readlines()
 
     # Розділимо дані на рядки через кому
@@ -21,17 +21,17 @@ try:
 
     for i in range(len(salary)):
         for j in range(1, len(salary[i])): # оскільки в першій клітинці ім`я
-            salary[i][j] = str(float(salary[i][j]) * usd_rate)
+            salary[i][j] = str(float(salary[i][j]) * grn_rate)
 
-    usd_salary = "converted_file.csv"
+    grn_salary = "salaries_uah.csv"
 
-    with open(usd_salary, "w") as file:
+    with open(grn_salary, "w") as file:
         file.write(",".join(header) + "\n") # Знову записали заголовок
 
         for row in salary:
             file.write(",".join(row) + "\n")
 
-    print("Your salary is converted to usd")
+    print("Your salary is converted to grn")
 
 
 except Exception as error:
